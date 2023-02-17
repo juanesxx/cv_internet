f2d80ffa73fe7dd2789d5f1a2903f638a9702d22
Juan Esteban <jmorales@bpogs.com>
2023-02-17 07:55:38 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.get('', (req, res) => {
+    res.json({ msg: 'OK' });
+});
+
+
+app.listen(3000);
